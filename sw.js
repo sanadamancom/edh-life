57e323af18c85375a414ab5d5a943c8be5400152
佐奈田万次郎 <176906943+sanadamancom@users.noreply.github.com>
2026-09-15 23:57:16 +0900

    Cache expanded dice menu assets

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const CACHE_NAME='edh-life-v37-material-help';
+const CACHE_NAME='edh-life-v38-more-dice';
 const APP_SHELL=[
   './',
   './index.html',
   './styles.css?v=33',
-  './counters.css?v=33',
+  './counters.css?v=34',
   './layout.js?v=30',
   './app.js?v=33',
+  './dice-extra.js?v=1',
   './manifest.webmanifest',
   './icon.svg'
 ];
