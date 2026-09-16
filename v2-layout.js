34a4734199029eba3f721072700a78220add8d56
佐奈田万次郎 <176906943+sanadamancom@users.noreply.github.com>
2026-09-17 00:23:38 +0900

    Bump compact layout stylesheet version

--- v2-layout.js
+++ v2-layout.js
@@ -73,7 +73,7 @@
   if(!document.querySelector('link[data-v2-compact]')){
     const link=document.createElement('link');
     link.rel='stylesheet';
-    link.href='./v2-compact.css?v=6';
+    link.href='./v2-compact.css?v=7';
     link.dataset.v2Compact='1';
     head.appendChild(link);
   }
