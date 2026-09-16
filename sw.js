417230a8d880b6475d8375cabd795ef116b2c5df
佐奈田万次郎 <176906943+sanadamancom@users.noreply.github.com>
2026-09-17 02:23:45 +0900

    Move life hold progress into panel highlight (#36)
    * Move life hold progress into player highlight
    
    * Disable legacy life hold indicator in v2
    
    * Bump v2 highlight asset versions
    
    * Refresh cache for hold progress highlight

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='edh-life-v70-shared-player-highlight';
+const CACHE_NAME='edh-life-v71-hold-progress-highlight';
 const APP_SHELL=[
   './',
   './index.html',
@@ -7,8 +7,8 @@ const APP_SHELL=[
   './v2.css?v=1',
   './v2-base.css?v=1',
   './v2-compact.css?v=13',
-  './v2-feedback.css?v=1',
-  './v2-utility.css?v=1',
+  './v2-feedback.css?v=2',
+  './v2-utility.css?v=2',
   './layout.js?v=30',
   './app.js?v=33',
   './edh-features.js?v=2',
