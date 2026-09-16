a0f246c9d9ae883eaaca0565f8cb9dca80bf3549
佐奈田万次郎 <176906943+sanadamancom@users.noreply.github.com>
2026-09-16 23:20:08 +0900

    Bust compact layout stylesheet cache

--- v2-layout.js
+++ v2-layout.js
@@ -5,7 +5,7 @@
   if(!document.querySelector('link[data-v2-compact]')){
     const link=document.createElement('link');
     link.rel='stylesheet';
-    link.href='./v2-compact.css?v=2';
+    link.href='./v2-compact.css?v=3';
     link.dataset.v2Compact='1';
     head.appendChild(link);
   }
